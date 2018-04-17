1ced6c7ea28d8617d87cc4d0744346d1bff7f358
Li Kai <hello+github@lik.ai>
2018-04-18 01:38:46 +0800

    Add comments page

--- src/Main.elm
+++ src/Main.elm
@@ -2,13 +2,20 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (class, href, src)
+import Html.Events exposing (onClick)
 import Html.Keyed
 import Http
 import Json.Decode as Decode
 import Json.Decode.Pipeline as Pipeline
+import Json.Encode as Encode
 
 
 ---- MODEL ----
+
+
+type Comments
+    = Comments Items
+    | Empty
 
 
 type alias Item =
@@ -19,6 +26,8 @@ type alias Item =
     , time : Int
     , timeAgo : String
     , comments_count : Int
+    , comments : Comments
+    , content : String
     , itemType : String
     , url : String
     , domain : String
@@ -31,6 +40,7 @@ type alias Items =
 
 type alias Model =
     { items : Items
+    , item : Maybe Item
     , page : Int
     , error : String
     }
@@ -38,23 +48,40 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { items = [], page = 1, error = "" }, fetchItems 1 )
+    ( { items = [], item = Nothing, page = 1, error = "" }, fetchItems 1 )
 
 
 
 ---- HTTP ----
 
 
+endpoint : String
+endpoint =
+    "https://api.hnpwa.com/v0"
+
+
 fetchItems : Int -> Cmd Msg
 fetchItems page =
     let
         url =
-            "https://api.hnpwa.com/v0/news/" ++ toString page ++ ".json"
+            endpoint ++ "/news/" ++ toString page ++ ".json"
 
         request =
             Http.get url decodeItems
     in
     Http.send NewItems request
+
+
+fetchItem : Int -> Cmd Msg
+fetchItem id =
+    let
+        url =
+            endpoint ++ "/item/" ++ toString id ++ ".json"
+
+        request =
+            Http.get url decodeItem
+    in
+    Http.send NewItem request
 
 
 decodeItems : Decode.Decoder Items
@@ -66,15 +93,22 @@ decodeItem : Decode.Decoder Item
 decodeItem =
     Pipeline.decode Item
         |> Pipeline.required "id" Decode.int
-        |> Pipeline.required "title" Decode.string
-        |> Pipeline.required "points" (Decode.nullable Decode.int)
-        |> Pipeline.required "user" (Decode.nullable Decode.string)
+        |> Pipeline.optional "title" Decode.string ""
+        |> Pipeline.optional "points" (Decode.nullable Decode.int) Nothing
+        |> Pipeline.optional "user" (Decode.nullable Decode.string) Nothing
         |> Pipeline.required "time" Decode.int
         |> Pipeline.required "time_ago" Decode.string
         |> Pipeline.required "comments_count" Decode.int
+        |> Pipeline.optional "comments" (Decode.lazy (\_ -> decodeComments)) Empty
+        |> Pipeline.optional "content" Decode.string ""
         |> Pipeline.required "type" Decode.string
         |> Pipeline.required "url" Decode.string
         |> Pipeline.optional "domain" Decode.string "https://news.ycombinator.com"
+
+
+decodeComments : Decode.Decoder Comments
+decodeComments =
+    Decode.map Comments (Decode.list (Decode.lazy (\_ -> decodeItem)))
 
 
 
@@ -83,7 +117,9 @@ decodeItem =
 
 type Msg
     = GetItems Int
+    | GetItem Int
     | NewItems (Result Http.Error Items)
+    | NewItem (Result Http.Error Item)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -92,8 +128,17 @@ update msg model =
         GetItems pageNum ->
             ( model, fetchItems pageNum )
 
+        GetItem id ->
+            ( model, fetchItem id )
+
         NewItems (Ok items) ->
             ( { model | items = items }, Cmd.none )
+
+        NewItem (Ok item) ->
+            ( { model | item = Just item }, Cmd.none )
+
+        NewItem (Err e) ->
+            ( { model | item = Nothing, error = toString e }, Cmd.none )
 
         NewItems (Err e) ->
             ( { model | error = toString e }, Cmd.none )
@@ -108,10 +153,15 @@ view model =
     div []
         [ viewNav
         , main_ [ class "main" ]
-            [ if model.error == "" then
-                Html.Keyed.ol [ class "items" ] (List.map viewItem model.items)
-              else
-                div [ class "box error" ] [ text "An error has occurred" ]
+            [ case ( model.item, model.error ) of
+                ( Nothing, "" ) ->
+                    Html.Keyed.ol [ class "items" ] (List.map viewListItem model.items)
+
+                ( Just item, "" ) ->
+                    viewItem item
+
+                ( _, err ) ->
+                    div [ class "box error" ] [ text err ]
             ]
         ]
 
@@ -124,16 +174,22 @@ viewNav =
         ]
 
 
-viewItem : Item -> ( String, Html msg )
-viewItem item =
+viewListItem : Item -> ( String, Html Msg )
+viewListItem item =
     ( toString item.id
     , li [ class "item-container" ]
-        [ article [ class "box item" ]
-            [ viewItemHeader item
-            , viewItemDetails item
-            ]
+        [ viewItem item
         ]
     )
+
+
+viewItem : Item -> Html Msg
+viewItem item =
+    article [ class "box item" ]
+        [ viewItemHeader item
+        , viewItemDetails item
+        , viewItemComments item.comments
+        ]
 
 
 viewItemHeader : Item -> Html msg
@@ -148,7 +204,7 @@ viewItemHeader item =
         ]
 
 
-viewItemDetails : Item -> Html msg
+viewItemDetails : Item -> Html Msg
 viewItemDetails item =
     let
         reputation =
@@ -161,8 +217,39 @@ viewItemDetails item =
     in
     div [ class "item-details" ]
         [ span [ class "item-poster" ] [ text (reputation ++ item.timeAgo) ]
-        , a [ class "item-comments" ] [ text (toString item.comments_count ++ " comments") ]
+        , a [ class "item-comments", onClick (GetItem item.id) ] [ text (toString item.comments_count ++ " comments") ]
         ]
+
+
+viewItemComments : Comments -> Html Msg
+viewItemComments comments =
+    ul [ class "comments" ] (List.map viewComment (getComments comments))
+
+
+viewComment : Item -> Html Msg
+viewComment item =
+    li []
+        [ div [ class "comment-meta" ]
+            [ text (Maybe.withDefault "" item.user ++ " " ++ item.timeAgo)
+            ]
+        , rawHtml div item.content
+        , viewItemComments item.comments
+        ]
+
+
+getComments : Comments -> List Item
+getComments comments =
+    case comments of
+        Comments items ->
+            items
+
+        Empty ->
+            []
+
+
+rawHtml : (List (Attribute Msg) -> List (Html Msg) -> Html Msg) -> String -> Html Msg
+rawHtml node htmlString =
+    node [ Html.Attributes.property "innerHTML" (Encode.string htmlString) ] []
 
 
 
